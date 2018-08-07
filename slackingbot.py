7a35814c3b2b0c323c206713f471b8c1cb243d8c
Danzel Taccayan (IS-PH) <Danzel_Taccayan@trendmicro.com>
2018-08-07 14:14:31 +0800

    Finalized slackbot, fixed variables and added comments

--- slackingbot.py
+++ slackingbot.py
@@ -6,16 +6,13 @@ import json
 import schedule
 from operator import itemgetter
 from slackclient import SlackClient
-
-
 slackClient = SlackClient(keys.slackBotKey)
 slackbotId = None
-
 rtmReadDelay = 1
-requestCommand = "tweet"
 MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
 
-def parseSlackingBotCommands(slackEvents):
+#Retrieves the messages of the SlackBot
+def parseResponse(slackEvents):
     for event in slackEvents:
         if event["type"] == "message" and not "subtype" in event:
             user_id, message = parseDM(event["text"])
@@ -23,14 +20,16 @@ def parseSlackingBotCommands(slackEvents):
                 return message, event["channel"]
     return None, None
 
+#Retrieves the ID and the message of a user
 def parseDM(message):
     matches = re.search(MENTION_REGEX, message)
     return (matches.group(1), matches.group(2).strip()) if matches else (None, None)
 
-def handle_command(command, channel):
-    notFoundResponse = "Command Not Found. Try tweet"
+#handles command sent by a user
+def handleCommand(command, channel):
+    notFoundResponse = "Command Not Found. Try sending in the command 'tweet'"
     response = None
-    if command.startswith(requestCommand):
+    if command.startswith("tweet"):
         response = showTweets()
 
     slackClient.api_call(
@@ -38,13 +37,12 @@ def handle_command(command, channel):
         channel=channel,
         text=response or notFoundResponse
     )
-
-def showTweets():
-    topTen = []
+#shows the top ten tweets (Globally)
+def showTweets(flag=False):
+    topTen = ["Top 10 Tweets of the Day"]
     auth = tweepy.OAuthHandler(keys.consumerKey,keys.consumerSecret)
     auth.set_access_token(keys.accessToken,keys.accessTokenSecret)
     api = tweepy.API(auth)
-
     trending = json.loads(json.dumps(api.trends_place(1),indent=2))
     counter = 0
     newTrending = trending[0]['trends']
@@ -56,30 +54,26 @@ def showTweets():
             counter+=1
 
     response = '\n'.join(topTen)
-    if channel == None:
+    if channel:
+        return response
+    else:
         slackClient.api_call(
             "chat.postMessage",
             channel='assignment1',
             text=response
        )
-    else:
-        return "\n".join(topTen)
-
-
-
-
 
 if __name__ == "__main__":
     if slackClient.rtm_connect(with_team_state=False):
         print("Slack Bot connected and running!")
         slackbotId = slackClient.api_call("auth.test")["user_id"]
-        schedule.every().day.at('00:00').do(showTweets)
+        schedule.every().day.at('11:56').do(showTweets)
         while True:
             schedule.run_pending()
             time.sleep(1)
-            command, channel = parseSlackingBotCommands(slackClient.rtm_read())
+            command, channel = parseResponse(slackClient.rtm_read())
             if command:
-                handle_command(command, channel)
+                handleCommand(command, channel)
             time.sleep(rtmReadDelay)
     else:
         print("Connection failed")
